8cd6c82dc0418772c1342131368e2467604936b4
EyabKabha <38509837+EyabKabha@users.noreply.github.com>
2018-11-28 23:30:35 +0200

    Add files via upload

---
+++ Cookie_HomeWork1/CookieHomeWork1/Cookie.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppHomeWork1
+{
+    class Cookie
+    {
+        private string gluten;
+
+        public string Gluten
+        {
+            get { return gluten; }
+            set { gluten = value; }
+        }
+
+       
+        private int eggs;
+
+        public int Eggs
+        {
+            get { return eggs; }
+            set { eggs = value; }
+        }
+
+        private int sugar;
+        public int Sugar
+        {
+            get { return sugar; }
+            set { sugar = value; }
+        }
+
+        public string amountflour;
+        public string Amountflour
+        {
+            get { return amountflour; }
+            set { amountflour = value; }
+        }
+        //public Cookie(string gluten,int eggs,int sugar,string AmountFlour)
+        //{
+        //    Gluten = gluten;
+        //    Eggs = eggs;
+        //    Sugar = sugar;
+        //    Amountflour = amountflour;
+        //}
+        //public Cookie() { }
+        public string Getinfo()
+        {
+            return $"Eggs :{Eggs}\nGluten :{Gluten}\nSugar {Sugar}\nFlour {Amountflour}";
+        }
+
+    }
+}
---
+++ Cookie_HomeWork1/CookieHomeWork1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ConsoleAppHomeWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" === Cookie number 1 ===");
+            Cookie c1 = new Cookie();
+            c1.Gluten = "Yes";
+            c1.Eggs = 6;
+            c1.Amountflour = "3G";
+            c1.Sugar = 15;
+            Console.WriteLine(c1.Getinfo());
+            Console.WriteLine(" === Cookie number 1 ===");
+            Cookie c2 = new Cookie();
+            c2.Gluten = "False";
+            c2.Eggs = 15;
+            c2.Amountflour = "35G";
+            c2.Sugar = 20;
+            Console.WriteLine(c2.Getinfo());
+            Console.WriteLine();
+            Console.WriteLine("");
+            Console.WriteLine("||== After Update The Sugar Amount ==||");
+            c1.Sugar = 60;
+            Console.WriteLine(c1.Getinfo());
+
+            c2 = c1;
+
+            c2.Eggs = 30;
+            Console.WriteLine(" === Update ===");
+            Console.WriteLine(c1.Getinfo());
+            Console.WriteLine(" === Cookie number 2 ===");
+            Console.WriteLine(c2.Getinfo());
+
+
+        } 
+    }
+}
