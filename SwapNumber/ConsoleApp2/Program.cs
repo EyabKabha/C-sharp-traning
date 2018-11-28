3d1e24865677b5fa43360036a007013a7e1d007f
EyabKabha <38509837+EyabKabha@users.noreply.github.com>
2018-11-28 23:33:15 +0200

    Csharp

---
+++ SwapNumber/ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Swap(int num1,int num2)
+        {
+            int temp;
+            temp = num1;
+            num1 = num2;
+            num2 = temp;
+           
+            Console.WriteLine($"after Swap --> {num1}  {num2}");
+        }
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            Console.WriteLine("Enter number");
+            num1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Secound number");
+             num2= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Before Swap --> {num1}  {num2}");
+            Swap(num1,num2);
+        }
+    }
+}
