8366bad2595288f8e697c07c900567355014d5fa
EyabKabha <38509837+EyabKabha@users.noreply.github.com>
2018-11-29 09:58:12 +0200

    Csharp

---
+++ Car_App_HomeWork_OOP/ConsoleAppOOP/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppOOP
+{
+    class Car
+    {
+        private int speed;
+
+        public  int Speed
+        {
+            get { return speed; }
+            set { speed = value; }
+        }
+
+        public string GetInfo()
+        {
+            return $"Your Car speed has: {speed} KM";
+        }
+
+    }
+}
---
+++ Car_App_HomeWork_OOP/ConsoleAppOOP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleAppOOP
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Car Car1 = new Car();
+            //2.1
+            Car1.Speed = 5;
+            //2.2
+            Console.WriteLine(Car1.GetInfo());
+            //2.3
+            Console.WriteLine("How much speed-up would you like to add?");
+            int usSpeed = Convert.ToInt32(Console.ReadLine());
+            Car1.Speed += usSpeed;
+            //2.4
+            Console.WriteLine(Car1.GetInfo());
+
+            //2.5 + 2.6
+            Console.WriteLine("Speed Lower -2 KM");
+            Car1.Speed -= 2;
+            Console.WriteLine(Car1.GetInfo());
+            //2.7
+            Console.WriteLine("Stop The Car!!");
+            Car1.Speed = 0;
+            Console.WriteLine(Car1.GetInfo());
+
+
+        }
+    }
+}
