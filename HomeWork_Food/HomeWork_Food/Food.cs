08b0fe84c7449ffb307b2ed77538ba2324cb4439
EyabKabha <38509837+EyabKabha@users.noreply.github.com>
2018-12-19 22:57:23 +0200

    Csharp

---
+++ HomeWork_Food/HomeWork_Food/Food.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork_Food
+{
+    class Food
+    {
+        private string productName;
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+        private int price;
+
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+        private bool kasher;
+
+        public bool Kasher
+        {
+            get { return kasher; }
+            set { kasher = value; }
+        }
+        private string manufacturername;
+
+        public string Manufacturername
+        {
+            get { return manufacturername; }
+            set { manufacturername = value; }
+        }
+        private int expirationyear;
+
+        public int Expirationyear
+        {
+            get { return expirationyear; }
+            set { expirationyear = value; }
+        }
+        public Food(string productName, int price, bool kasher, string manufacturername, int expirationyear)
+        {
+            ProductName = productName;
+            Price = price;
+            Kasher = kasher;
+            Manufacturername = manufacturername;
+            Expirationyear = expirationyear;
+        }
+        public string GetInfo()
+        {
+            return $"productName : {ProductName}\nPrice : {Price}\nmanufacturername : {Manufacturername}\nkosher : {Kasher}\nexpirationyear : {Expirationyear}";
+
+        }
+        public string expDatePass()
+        {
+            if (Expirationyear <= 2018)
+                return $"Yes";
+            else
+                return $"No";
+        }
+    }
+}
---
+++ HomeWork_Food/HomeWork_Food/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HomeWork_Food
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Food[] arr= new Food[5];
+            arr[0] = new Food("Coffe", 35, true, "Osim", 2019);
+            arr[1] = new Food("apple juice", 15, false, "Bregat", 2018);
+            arr[2] = new Food("Sugar", 20, true, "Osim", 2020);
+            arr[3] = new Food("cigarette", 40, false, "Bregat", 2015);
+            arr[4] = new Food("Bamba", 10, true, "Osim", 2019);
+
+            for (int i = 0; i <arr.Length; i++)
+            {
+                Console.WriteLine($"========= Item {i + 1} =========");
+                Console.WriteLine(arr[i].GetInfo());
+                Console.WriteLine("Is expire day ? " + arr[i].expDatePass());
+              
+            }
+        }
+    }
+}
